81f52fc9e4bce98a0ace00064f34f5cf00cc8a56
marianosacz <mariano.saczkowski@gmail.com>
2022-05-07 23:22:37 -0300

    se agrega gitignore

--- Challenge/Data/Conexion.cs
+++ Challenge/Data/Conexion.cs
@@ -7,6 +7,6 @@ namespace Challenge.Data
 {
     public class Conexion
     {
-        public static string rutaConexion = @"Data Source=BP4-W-122\SQLEXPRESS;Initial Catalog=DBPRUEBAS;Persist Security Info=True;User ID=test;PASSWORD=T3sting32;TrustServerCertificate=True";
+        public static string rutaConexion = @"Data Source=DESKTOP-QR03L4E;Initial Catalog=DBPRUEBAS;Integrated Security=True;TrustServerCertificate=True";
     }
 }
